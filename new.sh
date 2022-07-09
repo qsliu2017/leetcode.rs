574907b9626366b946ce704f1a6b134599a81bbe
qsliu <qsliu2017@outlook.com>
2022-07-10 01:32:34 +0800

    fix template

--- new.sh
+++ new.sh
@@ -4,7 +4,7 @@ lib=src/lib.rs
 new(){
     local mod=$(printf "_%04d" "$1")
     
-    cp -n template src/$mod.rs || echo "src/$mod.rs already exists" && exit 1
+    cp -n template src/$mod.rs || (echo "src/$mod.rs already exists" && exit 1)
     
     echo "mod $mod;" >> $lib
     sort $lib -o $lib.tmp
