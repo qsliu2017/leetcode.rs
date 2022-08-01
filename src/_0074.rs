772792f9a3e2a4b87f2a0893b3943d8c9d5123d2
qsliu <qsliu2017@outlook.com>
2022-08-01 20:26:48 +0800

    improve 74 (functional!)

--- src/_0074.rs
+++ src/_0074.rs
@@ -2,36 +2,23 @@ struct Solution;
 
 impl Solution {
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        matrix
+            .iter()
+            .flatten()
+            .collect::<Vec<_>>()
+            .binary_search(&&target)
+            .is_ok()
+    }
+
+    fn search_matrix_inplace(matrix: Vec<Vec<i32>>, target: i32) -> bool {
         let (m, n) = (matrix.len(), matrix[0].len());
-        let (mut left, mut right) = (0, m);
-        while left < right {
-            let mid = (left + right) / 2;
-            match target.cmp(&matrix[mid][0]) {
-                std::cmp::Ordering::Equal => return true,
-                std::cmp::Ordering::Less => right = mid,
-                std::cmp::Ordering::Greater => left = mid + 1,
-            };
-        }
-        if left == 0 {
-            return false;
-        }
-        let row = &matrix[left - 1];
-        let (mut left, mut right) = (0, n);
-        while left < right {
-            let mid = (left + right) / 2;
-            match target.cmp(&row[mid]) {
-                std::cmp::Ordering::Equal => {
-                    return true;
-                }
-                std::cmp::Ordering::Less => right = mid,
-                std::cmp::Ordering::Greater => left = mid + 1,
-            };
-        }
-        if left < n {
-            row[left] == target
-        } else {
-            false
-        }
+        (0..m * n)
+            .collect::<Vec<_>>()
+            .binary_search_by(|&idx| {
+                let (i, j) = (idx / n, idx % n);
+                matrix[i][j].cmp(&target)
+            })
+            .is_ok()
     }
 }
 #[cfg(test)]
@@ -51,6 +38,7 @@ mod tests {
                 13,
                 false,
             ),
+            (vec![vec![1, 1]], 0, false),
         ];
         for (matrix, target, output) in tt {
             assert_eq!(Solution::search_matrix(matrix, target), output);
