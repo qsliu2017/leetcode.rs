735240a05da6a9b71a27378f6d2c2794ea2e5e9d
qsliu <qsliu2017@outlook.com>
2024-06-05 16:56:13 +0800

    3072 binary indexed tree
    ref: https://en.wikipedia.org/wiki/Fenwick_tree

--- src/_3072.rs
+++ src/_3072.rs
@@ -1,23 +1,45 @@
 struct Solution;
-use std::cmp::Ordering;
+use std::{cmp::Ordering, collections::HashMap};
 impl Solution {
     pub fn result_array(nums: Vec<i32>) -> Vec<i32> {
         let n = nums.len();
+        let mut sorted = nums.clone();
+        sorted.sort_unstable_by(|a, b| b.cmp(a));
+        let indexes = sorted
+            .into_iter()
+            .enumerate()
+            .fold(HashMap::new(), |mut indexes, (i, n)| {
+                indexes.insert(n, i);
+                indexes
+            });
+        let add = |bitree: &mut Vec<i32>, mut index: usize| {
+            index += 1;
+            while index < n {
+                bitree[index] += 1;
+                index += index & (-(index as isize) as usize);
+            }
+        };
+        let get = |bitree: &Vec<i32>, mut index: usize| -> i32 {
+            let mut s = 0;
+            while index > 0 {
+                s += bitree[index];
+                index -= index & (-(index as isize) as usize);
+            }
+            s
+        };
         let (arr1, arr2, _, _) = nums.into_iter().enumerate().fold(
             (
                 Vec::with_capacity(n),
                 Vec::with_capacity(n),
-                Vec::with_capacity(n),
-                Vec::with_capacity(n),
+                vec![0; n + 1],
+                vec![0; n + 1],
             ),
             |(mut arr1, mut arr2, mut sorted1, mut sorted2), (i, n)| {
+                let index = *indexes.get(&n).unwrap();
                 let (arr, sorted) = match i {
                     0 => (&mut arr1, &mut sorted1),
                     1 => (&mut arr2, &mut sorted2),
-                    _ => match sorted1
-                        .partition_point(|&x| x > n)
-                        .cmp(&sorted2.partition_point(|&x| x > n))
-                    {
+                    _ => match get(&sorted1, index).cmp(&get(&sorted2, index)) {
                         Ordering::Greater => (&mut arr1, &mut sorted1),
                         Ordering::Less => (&mut arr2, &mut sorted2),
                         Ordering::Equal => {
@@ -30,7 +52,7 @@ impl Solution {
                     },
                 };
                 arr.push(n);
-                sorted.insert(sorted.partition_point(|&x| x > n), n);
+                add(sorted, index);
                 (arr1, arr2, sorted1, sorted2)
             },
         );
